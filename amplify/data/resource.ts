2057f757a7d57e75ac2d49b9d5cba72c4ebdc42e
Chongolnee <roongrunchai.chongolnee@philips.com>
2025-05-29 16:37:23 -0700

    removed the to-do functionality but kept login and logout

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,15 +1,13 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
 /*== STEP 1 ===============================================================
-The section below creates a Todo database table with a "content" field. Try
-adding a new "isDone" field as a boolean. The authorization rule below
-specifies that any user authenticated via an API key can "create", "read",
-"update", and "delete" any "Todo" records.
+Define your schema below. You can define additional models as needed.
 =========================================================================*/
 const schema = a.schema({
-  Todo: a
+  // We need at least one model to avoid syntax errors
+  EmptyModel: a
     .model({
-      content: a.string(),
+      id: a.id(),
     })
     .authorization((allow) => [allow.publicApiKey()]),
 });
@@ -44,14 +42,3 @@ import type { Schema } from "@/amplify/data/resource";
 
 const client = generateClient<Schema>() // use this Data client for CRUDL requests
 */
-
-/*== STEP 3 ===============================================================
-Fetch records from the database and use them in your frontend component.
-(THIS SNIPPET WILL ONLY WORK IN THE FRONTEND CODE FILE.)
-=========================================================================*/
-
-/* For example, in a React component, you can use this snippet in your
-  function's RETURN statement */
-// const { data: todos } = await client.models.Todo.list()
-
-// return <ul>{todos.map(todo => <li key={todo.id}>{todo.content}</li>)}</ul>
