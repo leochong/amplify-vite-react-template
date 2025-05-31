8fa2919a173df8e6f7e767caf6499718620b69c3
Chongolnee <roongrunchai.chongolnee@philips.com>
2025-05-31 10:30:35 -0700

    2nd attempt to add AI

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,60 @@
-import { useAuthenticator } from '@aws-amplify/ui-react';
+import * as React from 'react';
+import { useAuthenticator, Flex, TextAreaField, Loader, Text, View, Button } from '@aws-amplify/ui-react';
 import { StorageManager } from '@aws-amplify/ui-react-storage';
 import '@aws-amplify/ui-react/styles.css';
+import { useAIGeneration } from "./client";
 
 function App() {
   const { signOut } = useAuthenticator();
+  const [description, setDescription] = React.useState("");
+  const [{ data, isLoading }, generateRecipe] = useAIGeneration("generateRecipe");
+
+  const handleClick = async () => {
+    generateRecipe({ description });
+  };
 
   return (
     <main>
       <h1>My App</h1>
-      <div>
-        <h2>File Upload</h2>
-        <StorageManager
-          acceptedFileTypes={['image/*', 'application/pdf']}
-          accessLevel="private"
-          maxFileCount={5}
-          isResumable
-        />
-        <br />
-        🥳 App successfully hosted.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-      <button onClick={signOut}>Sign out</button>
+      <Flex direction="column">
+        <div>
+          <h2>File Upload</h2>
+          <StorageManager
+            acceptedFileTypes={['image/*', 'application/pdf']}
+            accessLevel="private"
+            maxFileCount={5}
+            isResumable
+          />
+        </div>
+        
+        <Flex direction="row">
+          <TextAreaField
+            autoResize
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            label="Description"
+          />
+          <Button onClick={handleClick}>Generate recipe</Button>
+        </Flex>
+        
+        {isLoading ? (
+          <Loader variation="linear" />
+        ) : (
+          <>
+            <Text fontWeight="bold">{data?.name}</Text>
+            <View as="ul">
+              {data?.ingredients?.map((ingredient) => (
+                <View as="li" key={ingredient}>
+                  {ingredient}
+                </View>
+              ))}
+            </View>
+            <Text>{data?.instructions}</Text>
+          </>
+        )}
+        
+        <Button onClick={signOut}>Sign out</Button>
+      </Flex>
     </main>
   );
 }
