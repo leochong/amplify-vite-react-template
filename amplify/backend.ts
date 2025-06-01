8f66cca66be5db35684e5c17b263ee73d0d634b0
Chongolnee <roongrunchai.chongolnee@philips.com>
2025-06-01 11:54:59 -0700

    removed ai portion

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,7 +2,7 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { firstBucket } from './storage/resource';
-// needed to add AI in the future
+
 defineBackend({
   auth,
   data,
